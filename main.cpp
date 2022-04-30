9a59f591423b792669140aa80b0d691a9f36b577
modmuss50 <modmuss50@gmail.com>
2022-04-30 20:11:20 +0100

    Windows subsystem

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "Bootstrap.h"
 #include "SystemHelper.h"
 
-int main() {
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 	const auto systemHelper = std::make_shared<SystemHelper>();
 
 	auto bs = Bootstrap(systemHelper);
