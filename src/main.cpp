d2b8ae5e9ea89958d44cb606861557bca49735e1
modmuss50 <modmuss50@gmail.com>
2022-05-18 12:41:53 +0100

    Prevent against DLL Injection and enable HeapEnableTerminationOnCorruption

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include "SystemHelper.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	// https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapsetinformation
+	HeapSetInformation(GetProcessHeap(), HeapEnableTerminationOnCorruption, NULL, 0);
+	// We dont want to try and load DLLs from the current directory (Quite often the downloads dir)
+	SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_SYSTEM32);
+
 	const auto systemHelper = std::make_shared<SystemHelper>();
 
 	auto bs = Bootstrap(systemHelper);
